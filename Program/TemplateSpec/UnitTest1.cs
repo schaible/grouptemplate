1b2ee93dd501dd1e5029205dfeb1c0acca1225d8
schaible <sarah.schaible@gmail.com>
2016-05-12 12:02:16 -0400

    Framework setup correctly first test passing

---
+++ Program/TemplateSpec/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemplateClassNameSpace;
+namespace TemplateSpec
+{
+    [TestClass]
+    public class TemplateClass
+    {
+        [TestMethod]
+        public void True_Is_True()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
